7b2d6cc505bdf0eb5388c36e06210a913fd05f01
ulvi_199517 <ulvi199517@gmail.com>
2020-08-11 20:30:57 -0400

    change response

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req,res)=> {
-    res.send(database.users);
+    res.send('it is working');
 })
 app.post('/signin', signin.handleSignin(db,bcrypt));
 app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)});
