b9b1c37aa71675fa1dda2b27d15c42c376887325
ulvi_199517 <ulvi199517@gmail.com>
2020-08-16 13:16:32 -0400

    change connectString to connectionString in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/handleImage');
 const db = knex({
     client: 'pg',
     connection: {
-      connectString : process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: {
       rejectUnauthorized: false,
       },
